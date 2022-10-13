d2636a693e8cab2333f411d90511bf5767e0a569
Dimitris Charalampidopoulos <dcharalab@gmail.com>
2022-10-13 16:42:37 +0300

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <file-selector v-model="files">
+      <dropzone v-slot="{ hovered }">
+        <div
+          class="p-2 block h-screen w-full rounded-lg border-4 border-dashed border-gray-400 transition-colors duration-150 flex flex-col justify-center items-center"
+          :class="{ 'border-blue-200': hovered }"
+        >
+          <div
+            class="bg-white rounded-lg border border-gray-200 w-96 text-gray-900 w-full"
+          >
+            <div
+              class="px-6 py-2 border-b border-gray-200 w-full"
+              v-for="file in files"
+              :key="file.name"
+            >
+              {{ file.name }}
+            </div>
+          </div>
+          <dialog-button
+            class="bg-indigo-500 hover:bg-indigo-400 transition-colors duration-150 rounded text-white px-4 py-2"
+          >
+            Επιλογή Αρχείων...
+          </dialog-button>
+        </div>
+      </dropzone>
+    </file-selector>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import { FileSelector, Dropzone, DialogButton } from "vue3-file-selector";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    FileSelector,
+    Dropzone,
+    DialogButton,
+  },
+  setup() {
+    const files = ref([]);
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    return {
+      files,
+    };
+  },
+};
+</script>
